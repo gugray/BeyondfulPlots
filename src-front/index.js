05db292297f4e0daebee2283d24ff76c61b8dcd8
gugray <gabor@berlin.de>
2017-07-20 19:26:31 +0200

    removed router

--- src-front/index.js
+++ src-front/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
+import AppData from './logic/AppData'
 import registerDefaultPlotters from './plotters/DefaultPlotters'
 import './styles/app.less'
 
 registerDefaultPlotters();
 
-render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    {/*
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
-    */}
-  </Router>
+render ((
+  <App data={window.AppData} />
 ), document.getElementById('app'))
